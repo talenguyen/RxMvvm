c2a93829eae9f44ab95fb9b66cf76f1abf4e28da
Giang Nguyen <giangnguyen.tale@gmail.com>
2016-06-13 05:45:24 +0700

    implement to flexible return

--- rxmvvm-sample/src/main/java/vn/tale/rxmvvm_sample/model/UserModel.java
+++ rxmvvm-sample/src/main/java/vn/tale/rxmvvm_sample/model/UserModel.java
@@ -3,27 +3,31 @@ package vn.tale.rxmvvm_sample.model;
 import java.util.List;
 
 import rx.Observable;
+import rx.functions.Func0;
 import vn.tale.rxmvvm_sample.api.GitHubService;
 
 /**
  * Created by Giang Nguyen at Tiki on 6/9/16.
  */
 public class UserModel {
+  private final GitHubService gitHubService;
+  private int count = -1;
 
-    private final GitHubService gitHubService;
-    private int count = 0;
+  public UserModel(GitHubService gitHubService) {
+    this.gitHubService = gitHubService;
+  }
 
-    public UserModel(GitHubService gitHubService) {
-        this.gitHubService = gitHubService;
-    }
-
-    public Observable<List<User>> getUsers() {
+  public Observable<List<User>> getUsers() {
+    return Observable.defer(new Func0<Observable<List<User>>>() {
+      @Override public Observable<List<User>> call() {
         count++;
         if (count % 3 == 0) {
-            return Observable.just(null);
+          return Observable.just(null);
         } else if (count % 3 == 1) {
-            return Observable.error(new RuntimeException("Error"));
+          return Observable.error(new RuntimeException("Error"));
         }
         return gitHubService.getUsers();
-    }
+      }
+    });
+  }
 }
