bbb5ecb073bf5c054d01f01cb069709c9c681a3f
Giang Nguyen <giangnguyen.tale@gmail.com>
2016-06-13 05:45:41 +0700

    update

--- rxmvvm-sample/src/main/java/vn/tale/rxmvvm_sample/ui/users/UsersFragment.java
+++ rxmvvm-sample/src/main/java/vn/tale/rxmvvm_sample/ui/users/UsersFragment.java
@@ -1,6 +1,5 @@
 package vn.tale.rxmvvm_sample.ui.users;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,12 +8,14 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import rx.Observable;
 import rx.Subscription;
 import rx.functions.Action1;
@@ -34,81 +35,81 @@ import static com.artemzin.rxui.RxUi.ui;
  */
 public class UsersFragment extends Fragment implements UsersMVVM.View {
 
-    @BindView(R.id.rvList)
-    RecyclerView rvList;
+  @BindView(R.id.rvList)
+  RecyclerView rvList;
+  @BindView(R.id.vProgress) ProgressBar vProgress;
+  @BindView(R.id.tvError) TextView tvError;
 
-    private UsersAdapter adapter;
-    private CompositeSubscription subscription;
-    public ProgressDialog loadingDialog;
+  private UsersAdapter adapter;
+  private CompositeSubscription subscription;
+  public UsersViewModel viewModel;
 
-    public UsersFragment() {
-        adapter = new UsersAdapter();
-    }
+  public UsersFragment() {
+    adapter = new UsersAdapter();
+  }
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_users, container, false);
-        ButterKnife.bind(this, view);
-        super.onCreateView(inflater, container, savedInstanceState);
-        return view;
-    }
+  @Nullable
+  @Override
+  public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.fragment_users, container, false);
+    ButterKnife.bind(this, view);
+    super.onCreateView(inflater, container, savedInstanceState);
+    return view;
+  }
 
-    @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        final UserModel userModel = new UserModel(GitHubService.Factory.create());
-        final UsersViewModel viewModel = new UsersViewModel(userModel, Schedulers.io());
-        final UsersBinding usersBinding = new UsersBinding(viewModel);
-        rvList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
-        rvList.setAdapter(adapter);
-        subscription = new CompositeSubscription();
-        subscription.add(usersBinding.bind(UsersFragment.this));
-        viewModel.loadUsers();
-    }
+  @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    final UserModel userModel = new UserModel(GitHubService.Factory.create());
+    viewModel = new UsersViewModel(userModel, Schedulers.io());
+    final UsersBinding usersBinding = new UsersBinding(viewModel);
+    rvList.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+    rvList.setAdapter(adapter);
+    subscription = new CompositeSubscription();
+    subscription.add(usersBinding.bind(UsersFragment.this));
+    viewModel.loadUsers();
+  }
 
-    @Override public void onDestroyView() {
-        super.onDestroyView();
-        subscription.unsubscribe();
-    }
+  @Override public void onDestroyView() {
+    super.onDestroyView();
+    subscription.unsubscribe();
+  }
 
-    @Override public Func1<Observable<Void>, Subscription> onLoading() {
-        return ui(new Action1<Void>() {
-            @Override public void call(Void aVoid) {
-                showLoading();
-            }
-        });
-    }
+  @OnClick(R.id.tvError) public void reload() {
+    viewModel.loadUsers();
+  }
 
-    private void showLoading() {
-        loadingDialog = ProgressDialog.show(getContext(), null, "Loading", true, false);
-    }
+  @Override public Func1<Observable<Void>, Subscription> onLoading() {
+    return ui(new Action1<Void>() {
+      @Override public void call(Void aVoid) {
+        vProgress.setVisibility(View.VISIBLE);
+        tvError.setVisibility(View.GONE);
+        rvList.setVisibility(View.GONE);
+      }
+    });
+  }
 
-    void hideLoading() {
-        if (loadingDialog != null) {
-            loadingDialog.dismiss();
-        }
-    }
+  @Override
+  public Func1<Observable<List<User>>, Subscription> onLoadUsersSuccess() {
+    return ui(new Action1<List<User>>() {
+      @Override
+      public void call(List<User> data) {
+        vProgress.setVisibility(View.GONE);
+        rvList.setVisibility(View.VISIBLE);
+        adapter.setUsers(data);
+        adapter.notifyDataSetChanged();
+      }
+    });
+  }
 
-    @Override
-    public Func1<Observable<List<User>>, Subscription> onLoadUsersSuccess() {
-        return ui(new Action1<List<User>>() {
-            @Override
-            public void call(List<User> data) {
-                hideLoading();
-                adapter.setUsers(data);
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-    @Override
-    public Func1<Observable<String>, Subscription> onLoadUsersError() {
-        return ui(new Action1<String>() {
-            @Override
-            public void call(String error) {
-                hideLoading();
-                Toast.makeText(getContext(), error, Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
+  @Override
+  public Func1<Observable<String>, Subscription> onLoadUsersError() {
+    return ui(new Action1<String>() {
+      @Override
+      public void call(String error) {
+        vProgress.setVisibility(View.GONE);
+        tvError.setVisibility(View.VISIBLE);
+        tvError.setText(error);
+      }
+    });
+  }
 }
